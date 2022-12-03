a7cf3e924c735d3c676ae45a2f0f75c09958075a
Abhijeeth Padarthi <abhijeeth@psu.edu>
2022-12-03 11:57:49 -0500

    changed ard log

--- trigger_arduino_servo/trigger_arduino_servo.ino
+++ trigger_arduino_servo/trigger_arduino_servo.ino
@@ -4,15 +4,14 @@
 #define STATUS_OFF 0
 #define STATUS_ON 1
 #define STATUS_RESET 2
-// #define STATUS_RUN_FIXED_CYCLES 3
+#define STATUS_RUN_FIXED_CYCLES 3
 
 Servo myservo;  
 int pos = 0;   
 int divfactor = 4;
 
 int current_status = STATUS_OFF;
-
-// int loop_num = 0;
+int loop_num = 0;
 
 
 void setup() {
@@ -26,7 +25,7 @@ void loop() {
   // change status if command trigerred
   if(Serial.available() > 0) {
     current_status = Serial.parseInt();
-    Serial.println("Status changed to " + String(current_status));
+    Serial.println("Status changed to " + String(current_status));      
   }
 
   if(current_status == STATUS_OFF)
@@ -39,21 +38,21 @@ void loop() {
     return;
   }
 
-  // if(current_status == STATUS_RUN_FIXED_CYCLES) {
-  //   if(loop_num == 0) {
-  //     loop_num = Serial.parseInt();
-  //   } else if(loop_num == 1) {
-  //     loop_num = 0;
-  //     current_status = STATUS_OFF;
-  //     return;
-  //   } else {
-  //     loop_num -= 1;
-  //   }
-  // }
+  if(current_status == STATUS_RUN_FIXED_CYCLES) {
+    if(loop_num == 0) {
+      loop_num = Serial.parseInt();
+    } else if(loop_num == 1) {
+      loop_num = 0;
+      current_status = STATUS_OFF;
+      return;
+    } else {
+      loop_num -= 1;
+    }
+  }
     
-
+  Serial.println(String(pos));
+  delay(20);
   while (pos <= 80) { 
-    Serial.println(String(pos));
     myservo.write(pos++); delay(45/divfactor);
     myservo.write(pos++); delay(40/divfactor);
     myservo.write(pos++); delay(30/divfactor);
@@ -64,22 +63,25 @@ void loop() {
     myservo.write(pos++); delay(30/divfactor);
     myservo.write(pos++); delay(40/divfactor);
     myservo.write(pos++); delay(45/divfactor); 
+    Serial.println(String(pos));
     delay(20);
   }
+
+  Serial.println(String(pos));
   delay(20);
 
   while (pos >= 10) { 
+    myservo.write(pos--); delay(45/divfactor);
+    myservo.write(pos--); delay(40/divfactor);
+    myservo.write(pos--); delay(30/divfactor);
+    myservo.write(pos--); delay(15/divfactor);
+    myservo.write(pos--); delay(10/divfactor);
+    myservo.write(pos--); delay(10/divfactor);
+    myservo.write(pos--); delay(15/divfactor);
+    myservo.write(pos--); delay(30/divfactor);
+    myservo.write(pos--); delay(40/divfactor);
+    myservo.write(pos--); delay(45/divfactor);
     Serial.println(String(pos));
-    myservo.write(pos--); delay(45/divfactor);
-    myservo.write(pos--); delay(40/divfactor);
-    myservo.write(pos--); delay(30/divfactor);
-    myservo.write(pos--); delay(15/divfactor);
-    myservo.write(pos--); delay(10/divfactor);
-    myservo.write(pos--); delay(10/divfactor);
-    myservo.write(pos--); delay(15/divfactor);
-    myservo.write(pos--); delay(30/divfactor);
-    myservo.write(pos--); delay(40/divfactor);
-    myservo.write(pos--); delay(45/divfactor);
     delay(20);
   }
   delay(20);  
